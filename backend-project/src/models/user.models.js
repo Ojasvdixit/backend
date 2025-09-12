2fe25e0ab65aaa528dae20310a5e2f3747c1e89a
Ojasv Dixit <115476649+Ojasvdixit@users.noreply.github.com>
2025-09-12 23:13:52 +0530

    Add user and video models with JWT and bcrypt integration; implement password hashing and token generation

--- backend-project/src/models/user.models.js
+++ backend-project/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
   {
@@ -58,6 +60,55 @@ const userSchema = new Schema(
   timestamps: true
 })
 
+
+//? save is a type of middleware
+
+userSchema.pre('save', async function(next){
+  if(!this.isModified('password')) return next()         //! if password is not modified then return next()
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+
+})
+//! custom way  to create custom function using methods object
+userSchema.methods.isPasswordCorrect = async function(password){
+
+  return await bcrypt.compare(password, this.password)
+
+}
+
+userSchema.methods.generatAccessToken = function(){
+
+ return  jwt.sign(
+    {
+      _id:this._id,
+      username:this.username,
+      email:this.email,
+      fullname:this.fullname,
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+  
+ return  jwt.sign(
+    {
+      _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+
+}
 
 
 export const User = mongoose.model('User', userSchema)
